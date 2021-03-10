4c97033aa5b3459af7d257d1c1fa6f47a5c405e3
Pavan Hegde <pavan.hegde@us.gt.com>
2021-03-10 15:17:33 +0530

    Added Cards

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,10 +3,10 @@ import Navbar from "../components/Navbar";
 
 export default function Layout({ children, title }) {
   return (
-    <div className="flex flex-col h-screen justify-between bg-green-100">
+    <div className="flex flex-col min-h-screen justify-between bg-green-100">
       <Head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
       </Head>
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,13 +2,90 @@ import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <Layout title="Portfolio | Pavan">
-      <div className="flex flex-col items-center justify-center h-96 w-96 bg-green-50 rounded-xl shadow-lg transform hover:shadow-2xl">
-        <h1 className="text-4xl font-bold p-4 text-center">
-          Hey I'm Pavan <span className="text-4xl font-thin"> Hegde</span>
-        </h1>
-        <p className="text-xl font-thin mb-8">Consultant @ Grant Thornton LLP</p>
-        <p className="text-xl font-mono">Page under construction!</p>
+    <Layout title="CSR | Computer Skills Training">
+      <div className="grid grid-cols-3 grid-rows-auto gap-6">
+        <div className="flex flex-col items-center bg-green-200 shadow-lg p-4 rounded-lg transform hover:scale-105 hover:shadow-2xl transition ease-out duration-100 cursor-pointer">
+          <h1 className="text-3xl font-semibold text-gray-700 mb-6 leading-tight">
+            What is computer?
+          </h1>
+          <p className="text-lg font-medium text-gray-500 leading-relaxed flex flex-shrink-0">
+            A Computer is an electronic device. It was created by Charles
+            Babbage.{" "}
+          </p>
+          <p className="text-lg font-medium text-gray-500 leading-relaxed flex flex-shrink-0">
+            The word computer is taken from word "compute" which means
+            calculate.
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center bg-green-200 shadow-lg p-4 rounded-lg transform hover:scale-105 hover:shadow-2xl transition ease-out duration-100 cursor-pointer">
+          <h1 className="text-3xl font-semibold text-gray-700 mb-6 leading-relaxed">
+            Uses of computer
+          </h1>
+          <p className="text-lg font-medium text-gray-500 leading-relaxed flex flex-shrink-0">
+            A Computer is an electronic device. It was created by Charles
+            Babbage.{" "}
+          </p>
+          <p className="text-lg font-medium text-gray-500 leading-relaxed flex flex-shrink-0">
+            The word computer is taken from word "compute" which means
+            calculate.
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center bg-green-200 shadow-lg p-4 rounded-lg transform hover:scale-105 hover:shadow-2xl transition ease-out duration-100 cursor-pointer">
+          <h1 className="text-3xl font-semibold text-gray-700 mb-6 leading-relaxed">
+            Parts of computer
+          </h1>
+          <p className="text-lg font-medium text-gray-500 leading-relaxed flex flex-shrink-0">
+            A Computer is an electronic device. It was created by Charles
+            Babbage.{" "}
+          </p>
+          <p className="text-lg font-medium text-gray-500 leading-relaxed flex flex-shrink-0">
+            The word computer is taken from word "compute" which means
+            calculate.
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center bg-green-200 shadow-lg p-4 rounded-lg transform hover:scale-105 hover:shadow-2xl transition ease-out duration-100 cursor-pointer">
+          <h1 className="text-3xl font-semibold text-gray-700 mb-6 leading-relaxed">
+            Windows OS
+          </h1>
+          <p className="text-lg font-medium text-gray-500 leading-relaxed flex flex-shrink-0">
+            A Computer is an electronic device. It was created by Charles
+            Babbage.{" "}
+          </p>
+          <p className="text-lg font-medium text-gray-500 leading-relaxed flex flex-shrink-0">
+            The word computer is taken from word "compute" which means
+            calculate.
+          </p>
+        </div>
+        <div className="flex flex-col items-center bg-green-200 shadow-lg p-4 rounded-lg transform hover:scale-105 hover:shadow-2xl transition ease-out duration-100 cursor-pointer">
+          <h1 className="text-3xl font-semibold text-gray-700 mb-6 leading-relaxed">
+            Windows File Explorer.
+          </h1>
+          <p className="text-lg font-medium text-gray-500 leading-relaxed flex flex-shrink-0">
+            A Computer is an electronic device. It was created by Charles
+            Babbage.{" "}
+          </p>
+          <p className="text-lg font-medium text-gray-500 leading-relaxed flex flex-shrink-0">
+            The word computer is taken from word "compute" which means
+            calculate.
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center bg-green-200 shadow-lg p-4 rounded-lg transform hover:scale-105 hover:shadow-2xl transition ease-out duration-100 cursor-pointer">
+          <h1 className="text-3xl font-semibold text-gray-700 mb-6 leading-relaxed">
+            Tell US!!.
+          </h1>
+          <p className="text-lg font-medium text-gray-500 leading-relaxed flex flex-shrink-0">
+            A Computer is an electronic device. It was created by Charles
+            Babbage.{" "}
+          </p>
+          <p className="text-lg font-medium text-gray-500 leading-relaxed flex flex-shrink-0">
+            The word computer is taken from word "compute" which means
+            calculate.
+          </p>
+        </div>
       </div>
     </Layout>
   );
