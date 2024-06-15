a6f607f878f336828281c9b46df543cab6819728
Pavan Hegde <pavanrkadave@gmail.com>
2024-06-15 15:48:22 +0200

    Add Some info

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-black">
-      <div className="max-w-7xl mx-auto">
+      <div className="max-w-7xl mx-auto p-5 overflow-hidden">
         <Navbar />
+        <HeroSection />
       </div>
     </div>
   );
